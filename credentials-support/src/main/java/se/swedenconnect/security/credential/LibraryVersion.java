9e7c103cfdeda5bee6f900c8b644c5589ada8b58
Martin Lindström <martin.lindstrom@litsec.se>
2025-05-08 10:58:40 +0200

    2.0.6

--- credentials-support/src/main/java/se/swedenconnect/security/credential/LibraryVersion.java
+++ credentials-support/src/main/java/se/swedenconnect/security/credential/LibraryVersion.java
@@ -24,7 +24,7 @@ public final class LibraryVersion {
 
   private static final int MAJOR = 2;
   private static final int MINOR = 0;
-  private static final int PATCH = 5;
+  private static final int PATCH = 6;
 
   /**
    * Global serialization value for classes.
