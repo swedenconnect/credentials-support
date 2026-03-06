8c422bee93676864585a4516a406ab762eec799c
Martin Lindström <martin-lindstrom@users.noreply.github.com>
2026-03-06 15:04:44 +0100

    IS-113 Upgraded Spring versions (#114)

--- spring/credentials-support-spring-boot-starter/src/test/java/se/swedenconnect/security/credential/spring/actuator/CredentialMonitorHealthIndicatorTest.java
+++ spring/credentials-support-spring-boot-starter/src/test/java/se/swedenconnect/security/credential/spring/actuator/CredentialMonitorHealthIndicatorTest.java
@@ -16,8 +16,8 @@
 package se.swedenconnect.security.credential.spring.actuator;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.Status;
+import org.springframework.boot.health.contributor.Health;
+import org.springframework.boot.health.contributor.Status;
 import se.swedenconnect.security.credential.monitoring.DefaultCredentialMonitorBeanTest;
 import se.swedenconnect.security.credential.spring.actuator.CredentialMonitorHealthIndicator.MonitorStatus;
 import se.swedenconnect.security.credential.spring.monitoring.events.FailedCredentialReloadEvent;
